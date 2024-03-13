17315a87f541b2451656e61b534c48611dac97bf
MarkRBishop <mrb.8@hotmail.com>
2024-03-12 23:15:14 -0500

    added logos and links

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -15,6 +15,17 @@ const AboutMe = () => {
           <p className="lead">
             While the program is coming to an end, I'm ready to take the next step and continue learning more about this fascinating career.
           </p>
+          <div className="d-flex justify-content-around mt-4">
+            <a href="https://stackoverflow.com/users/23596630/mrb" target="_blank" rel="noopener noreferrer">
+              <img src="https://cdn.sstatic.net/Sites/stackoverflow/company/Img/logos/so/so-logo.png" alt="Stack Overflow" width="120" height="50" />
+            </a>
+            <a href="https://github.com/MarkRBishop" target="_blank" rel="noopener noreferrer">
+              <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" width="50" height="50" />
+            </a>
+            <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+              <img src="https://brand.linkedin.com/content/dam/brand/site/img/logo/logo-r.png" alt="LinkedIn" width="100" height="50" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
