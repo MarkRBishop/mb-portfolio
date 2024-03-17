ca41623467c0da069c7e5c2fed4f0e02566979cb
MarkRBishop <mrb.8@hotmail.com>
2024-03-16 19:58:48 -0500

    testing background image

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import backgroundImage from 'assets/abstract-luxury-gradient-blue-background-smooth-dark-blue-with-black-vignette-studio-banner_1258-87847.avif'
 
 import './App.css'
 
 const App = () => {
   return (
-    <div>
+    <div style={{ backgroundImage: `url(${backgroundImage})`, minHeight: '100vh', backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
       <Header />
       <main>
         <Outlet />
